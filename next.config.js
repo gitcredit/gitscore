9ce8f4e053b9ae16d2caf1a45de614465a83e7a2
rick <l.riccardo@web.de>
2023-03-03 03:27:43 +0100

    added score calculator

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    PAT: process.env.PAT,
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 
 export default function Component() {
   const { data: session } = useSession();
-  const [insights, setInsights] = useState();
   const [forkCount, setForkCount] = useState();
   const [followersForkCount, setFollowersForkCount] = useState();
   const [stargazerCount, setStargazerCount] = useState();
@@ -46,15 +45,40 @@ export default function Component() {
     }
   }, [session]);
 
+  const calcScore = () => {
+    const score =
+      mergedPullRequestCount365d +
+      mergedPullRequestCount30d +
+      mergedPullRequestCount / 4 +
+      stargazerCount * 4 +
+      forkCount;
+    return score + 455;
+  };
+
   if (session) {
     return (
       <div className="flex flex-col justify-center pt-10">
-        <div className="flex font-bold text-xl justify-center pb-3">
+        <div className="flex font-bold text-2xl justify-center pb-3">
           gitscore
         </div>
         <div className="flex flex-col justify-center">
           <div className="flex justify-center pb-3">
             Signed in as {session.user.email}
+          </div>
+          <div className="flex justify-center font-bold text-xl">
+            Your score micro signals
+          </div>
+          <div className="flex flex-col justify-center items-center pb-5">
+            <div>forkCount {forkCount}</div>
+            <div>followersForkCount {followersForkCount}</div>
+            <div>stargazerCount {stargazerCount}</div>
+            <div>followersFollowerCount {followersFollowerCount}</div>
+            <div>mergedPullRequestCount {mergedPullRequestCount}</div>
+            <div>mergedPullRequestCount30d {mergedPullRequestCount30d}</div>
+            <div>mergedPullRequestCount365d {mergedPullRequestCount365d}</div>
+          </div>
+          <div className="flex justify-center font-bold text-xl pb-5">
+            Total {calcScore()}
           </div>
           <div className="flex justify-center">
             <button className="border p-3" onClick={() => signOut()}>
