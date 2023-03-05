052d2b30391fec1cbd2ddb75af0cf00313b35407
Alp Guneysel <alp.guneysel@gmail.com>
2023-03-05 11:16:29 -0700

    debug score2

--- pages/score.js
+++ pages/score.js
@@ -91,6 +91,7 @@ export default function Score() {
     // createdAt                      |
     // avatarUrl                      | x
     // score                          | x
+console.log("calc_user_rank____");
 
     // List of weights each component has on the rank
     const weights = [4/65, 2/65, 4/65, 4/65, 4/65, 5/65, 5/65, 5/65, 6/65, 7/65, 7/65, 2/65, 2/65, 1/65, 1/65, 3/65, 3/65];
@@ -455,6 +456,200 @@ console.log("SCOREEE",calculatedscore);
               </div>
             </div>
           </div>
+
+
+          <div class="py-12 px-4">
+      <div class="lg:max-w-[356px] md:max-w-[516px] max-w-[343px] mx-auto">
+        <div class="mx-auto bg-white px-3 py-4 rounded">
+          <div>
+            <img
+              src="https://tuk-cdn.s3.amazonaws.com/can-uploader/Group%20813077.png"
+              class="mx-auto"
+            />
+          </div>
+          <div class="lg:block hidden">
+            <div class="flex justify-between items-center gap-x-4 px-8">
+              <div aria-label="one">
+                <div class="flex gap-2 items-center">
+                  <svg
+                    width="20"
+                    height="8"
+                    viewBox="0 0 20 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="20" height="8" rx="4" fill="#EF4444" />
+                  </svg>
+                  <p class="text-xs font-medium leading-3 text-gray-800">
+                    Poor
+                  </p>
+                </div>
+                <div class="flex items-center gap-2 mt-3">
+                  <svg
+                    width="20"
+                    height="8"
+                    viewBox="0 0 20 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="20" height="8" rx="4" fill="#FACC15" />
+                  </svg>
+
+                  <p class="text-xs font-medium leading-3 text-gray-800">
+                    Fair
+                  </p>
+                </div>
+                <div class="flex items-center gap-2 mt-3">
+                  <svg
+                    width="20"
+                    height="8"
+                    viewBox="0 0 20 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="20" height="8" rx="4" fill="#BEF264" />
+                  </svg>
+
+                  <p class="text-xs font-medium leading-3 text-gray-800">
+                    Satisfactory
+                  </p>
+                </div>
+              </div>
+              <div aria-label="two">
+                <div class="flex items-center gap-2">
+                  <svg
+                    width="20"
+                    height="8"
+                    viewBox="0 0 20 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="20" height="8" rx="4" fill="#14B8A6" />
+                  </svg>
+                  <p class="text-xs font-medium leading-3 text-gray-800">
+                    Good
+                  </p>
+                </div>
+                <div class="flex items-center gap-2 mt-3">
+                  <svg
+                    width="20"
+                    height="8"
+                    viewBox="0 0 20 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="20" height="8" rx="4" fill="#22C55E" />
+                  </svg>
+                  <p class="text-xs font-medium leading-3 text-gray-800">
+                    Better
+                  </p>
+                </div>
+                <div class="flex items-center gap-2 mt-3">
+                  <svg
+                    width="20"
+                    height="8"
+                    viewBox="0 0 20 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect width="20" height="8" rx="4" fill="#15803D" />
+                  </svg>
+                  <p class="text-xs font-medium leading-3 text-gray-800">
+                    Exceptional
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="lg:hidden block">
+            <div class="flex flex-wrap justify-between gap-3 items-center px-8">
+              <div class="flex gap-2 items-center">
+                <svg
+                  width="20"
+                  height="8"
+                  viewBox="0 0 20 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="20" height="8" rx="4" fill="#EF4444" />
+                </svg>
+                <p class="text-xs font-medium leading-3 text-gray-800">Poor</p>
+              </div>
+              <div class="flex items-center gap-2">
+                <svg
+                  width="20"
+                  height="8"
+                  viewBox="0 0 20 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="20" height="8" rx="4" fill="#FACC15" />
+                </svg>
+
+                <p class="text-xs font-medium leading-3 text-gray-800">Fair</p>
+              </div>
+              <div class="flex items-center gap-2">
+                <svg
+                  width="20"
+                  height="8"
+                  viewBox="0 0 20 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="20" height="8" rx="4" fill="#BEF264" />
+                </svg>
+
+                <p class="text-xs font-medium leading-3 text-gray-800">
+                  Satisfactory
+                </p>
+              </div>
+              <div class="flex items-center gap-2">
+                <svg
+                  width="20"
+                  height="8"
+                  viewBox="0 0 20 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="20" height="8" rx="4" fill="#14B8A6" />
+                </svg>
+                <p class="text-xs font-medium leading-3 text-gray-800">Good</p>
+              </div>
+              <div class="flex items-center gap-2">
+                <svg
+                  width="20"
+                  height="8"
+                  viewBox="0 0 20 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="20" height="8" rx="4" fill="#22C55E" />
+                </svg>
+                <p class="text-xs font-medium leading-3 text-gray-800">
+                  Better
+                </p>
+              </div>
+              <div class="flex items-center gap-2">
+                <svg
+                  width="20"
+                  height="8"
+                  viewBox="0 0 20 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="20" height="8" rx="4" fill="#15803D" />
+                </svg>
+                <p class="text-xs font-medium leading-3 text-gray-800">
+                  Exceptional
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
     </>
   )
 }
