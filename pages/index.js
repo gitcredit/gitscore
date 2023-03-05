6a764a0c9b6b325c879bf8c9060f0f44fb27ec94
Alp Guneysel <alp.guneysel@gmail.com>
2023-03-05 12:38:44 -0700

    edit template 2

--- pages/index.js
+++ pages/index.js
@@ -227,7 +227,120 @@ const footerNavigation = {
 
 export default function Homer() {
   return (
-   
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+        <Popover as="header" className="relative">
+          <div className="pt-6" style={{backgroundColor:"#7642bd"}}>
+            <nav className="relative mx-auto flex max-w-7xl items-center justify-between px-6" aria-label="Global">
+              <div className="flex flex-1 items-center">
+                <div className="flex w-full items-center justify-between md:w-auto">
+                  <a href="#">
+                    <span className="sr-only">gitcredit</span>
+                    <img
+                      className="h-8 w-auto sm:h-10"
+                      src="/gitcredit.svg"
+                      alt=""
+                    />
+                  </a>
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <Popover.Button className="focus-ring-inset inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white" style={{backgroundColor:"#7642bd"}}>
+                      <span className="sr-only">Open main menu</span>
+                      <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="hidden space-x-8 md:ml-10 md:flex">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-base font-medium text-white hover:text-gray-300"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:space-x-6">
+              
+                <a
+                  href="#"
+
+  onClick={() => {
+    signIn("github", {
+      callbackUrl: 'https://gitscore.vercel.app/score',
+    });
+  }}
+
+
+                 
+                  className="inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-black hover:bg-gray-700"
+                >
+                  Connect Github 
+                </a>
+              </div>
+            </nav>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top transform p-2 transition md:hidden">
+              <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?from-color=teal&from-shade=500&to-color=cyan&to-shade=600&toShade=600"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="pt-5 pb-6">
+                  <div className="space-y-1 px-2">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="mt-6 px-5">
+                    <a
+                      href="#"
+                      className="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700"
+                    >
+                      Start free trial
+                    </a>
+                  </div>
+                  <div className="mt-6 px-5">
+                    <p className="text-center text-base font-medium text-gray-500">
+                      Existing customer?{' '}
+                      <a href="#" className="text-gray-900 hover:underline">
+                        Login
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
         <main>
           <div className=" pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14" style={{backgroundColor:"#7642bd"}}>
             <div className="mx-auto max-w-7xl lg:px-8">
@@ -388,6 +501,93 @@ Try credit now!
             </div>
           </div>
         </main>
-       
+        <footer className="bg-gray-50" aria-labelledby="footer-heading">
+          <h2 id="footer-heading" className="sr-only">
+            Footer
+          </h2>
+          <div className="mx-auto max-w-md px-6 pt-12 sm:max-w-7xl lg:px-8 lg:pt-16">
+            <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+              <div className="space-y-8 xl:col-span-1">
+                <img
+                  className="h-10"
+                  src="/gitcredit.svg"
+                  alt="Company name"
+                />
+                <p className="text-base text-gray-500">
+                  Financing open-source innovation.
+                </p>
+                <div className="flex space-x-6">
+                  {footerNavigation.social.map((item) => (
+                    <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                      <span className="sr-only">{item.name}</span>
+                      <item.icon className="h-6 w-6" aria-hidden="true" />
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className="mt-12 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+                <div className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <h3 className="text-base font-medium text-gray-900">Solutions</h3>
+                    <ul role="list" className="mt-4 space-y-4">
+                      {footerNavigation.solutions.map((item) => (
+                        <li key={item.name}>
+                          <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div className="mt-12 md:mt-0">
+                    <h3 className="text-base font-medium text-gray-900">Support</h3>
+                    <ul role="list" className="mt-4 space-y-4">
+                      {footerNavigation.support.map((item) => (
+                        <li key={item.name}>
+                          <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <div className="md:grid md:grid-cols-2 md:gap-8">
+                  <div>
+                    <h3 className="text-base font-medium text-gray-900">Company</h3>
+                    <ul role="list" className="mt-4 space-y-4">
+                      {footerNavigation.company.map((item) => (
+                        <li key={item.name}>
+                          <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div className="mt-12 md:mt-0">
+                    <h3 className="text-base font-medium text-gray-900">Legal</h3>
+                    <ul role="list" className="mt-4 space-y-4">
+                      {footerNavigation.legal.map((item) => (
+                        <li key={item.name}>
+                          <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-12 border-t border-gray-200 py-8">
+              <p className="text-base text-gray-400 xl:text-center">
+                &copy; 2023 gitcredit, Inc. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
   )
 }
