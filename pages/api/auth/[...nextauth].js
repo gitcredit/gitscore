6b2b1eef3c7ab1d1ce5af711f55aee64205addb7
Alp Guneysel <alp.guneysel@gmail.com>
2023-03-04 22:51:25 -0700

    github extra data

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,94 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session({ session, user }) {
+
+console.log("SESSION",session);
+console.log("USER",user);
+      session.user.id = user.id
+      return session
+    },
+    async signIn(user, account, metadata) {
+console.log("USER",user);
+console.log("ACCOUNT",account);
+console.log("METADATA",metadata);
+
+
+if (account.provider === 'github') {    
+  console.log("METADATA",metadata);
+    const githubUser = {
+        id: metadata.id,
+        login: metadata.login,
+        name: metadata.name,
+        avatar: user.image
+    }
+
+    //user.accessToken = await getTokenFromYourAPIServer('github', githubUser)
+    return true
+}
+
+return false;
+
+
+
+    }
+  },
+
+});
+
+
+
+
+
+/*
+
+
+
+
+    // ...add more providers here
+
+async signIn(user, account, metadata) {
+      if (account.provider === 'github') {    
+        console.log("METADATA",metadata);
+          const githubUser = {
+              id: metadata.id,
+              login: metadata.login,
+              name: metadata.name,
+              avatar: user.image
+          }
+      
+          user.accessToken = await getTokenFromYourAPIServer('github', githubUser)
+          return true
+      }
+  
+      return false;
+  },
+ async  jwt(token, user) {
+    if (user) {
+        token = { accessToken: user.accessToken }
+    }
+
+    return token
+},
+async  session(session, token) {
+  session.accessToken = token.accessToken
+  return session
+}
+],
+
+
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github";
 
@@ -46,3 +137,5 @@ const options = {
 }
 
 export default (req, res) => NextAuth(req, res, options)
+
+*/
